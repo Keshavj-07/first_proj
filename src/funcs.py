2855fd4012771f62cb0d4ed39b91439934df801e
Keshav <keshavjhunjhunwala07@gmail.com>
2026-06-10 14:35:02 -0400

    Latest formatting changes in files

--- src/funcs.py
+++ src/funcs.py
@@ -1,6 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+
 
 def convert_wavelength_air2vacuum(wavelength_air):
     """
@@ -20,6 +19,7 @@ def convert_wavelength_air2vacuum(wavelength_air):
 
     return wavelength_air * fact
 
+
 def bass_compare(obs_x, obs_y, sim_x, sim_y, x_min=6061, x_max=7061):
     """
     Compare the observed solar spectrum with the STARDIS simulation
@@ -34,17 +34,23 @@ def bass_compare(obs_x, obs_y, sim_x, sim_y, x_min=6061, x_max=7061):
     --------
     None
     """
-    plt.figure(figsize=(10,6))
-    plt.plot(obs_x, obs_y, label='Observed Solar Spectrum', color = 'black', alpha = 0.6)
+    plt.figure(figsize=(10, 6))
+    plt.plot(
+        obs_x,
+        obs_y,
+        label='Observed Solar Spectrum',
+        color='black',
+        alpha=0.6,
+    )
     plt.xlim((x_min, x_max))
     plt.title("Observation vs STARDIS Solar Spectrum")
     plt.xlabel(r"Wavelength [$\AA$]")
     plt.ylabel(r"Flux density [erg/s/cm$^2$/$\AA$]")
     plt.tight_layout()
 
-    plt.plot (sim_x, sim_y, label='STARDIS Simulation')
-    #plt.vlines(6564.6, ymin=0, ymax=10000, color='red', label='H-alpha line')
-    #plt.vlines(6867.4, ymin=0, ymax=10000, color='red', label='O2 B line')
+    plt.plot(sim_x, sim_y, label='STARDIS Simulation')
+    # plt.vlines(6564.6, ymin=0, ymax=10000, color='red', label='H-alpha line')
+    # plt.vlines(6867.4, ymin=0, ymax=10000, color='red', label='O2 B line')
     plt.legend()
     plt.show()
 
@@ -63,7 +69,7 @@ def continuum_visualizer(x1, y1, x2, y2, x_min=6061, x_max=7061):
     --------
     None
     """
-    plt.figure(figsize=(10,6))
+    plt.figure(figsize=(10, 6))
     plt.plot(x2, y2, label='STARDIS Simulation')
     plt.xlim((x_min, x_max))
     plt.title("STARDIS Solar Spectrum Continuum")
@@ -71,6 +77,6 @@ def continuum_visualizer(x1, y1, x2, y2, x_min=6061, x_max=7061):
     plt.ylabel(r"Flux density [erg/s/cm$^2$/$\AA$]")
     plt.tight_layout()
 
-    plt.plot (x1, y1, label='Continuum', color='red')
+    plt.plot(x1, y1, label='Continuum', color='red')
     plt.legend()
     plt.show()
